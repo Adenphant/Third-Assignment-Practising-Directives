41ab803879bd7db9a441a0570df11151197dfd98
Alexander Hornstrup Pødenphant <aluxpoed@gmail.com>
2023-03-22 15:33:28 +0100

    Third Assignment: Practicing Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ThisAssignment';
+  title = 'Third Assignment';
+  showSecret = false;
+  clicks = [];
+  
+
+  onButtonClick(){
+    this.showSecret = !this.showSecret;
+    this.clicks.push(this.clicks.length + 1)
+  } 
+  // else {
+  //   this.text === 'Heyaaaa'
+  // }
 }
+// this.clicks.push('Test')
+// if(this.text != '')
+// this.text === ''
+// this.clicks.push(1 + 1);
